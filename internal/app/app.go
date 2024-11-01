5e6d38f31c2306cb585efc06dcf855adff2ffb11
nurbol0tt <fairtail764@gmail.com>
2024-11-01 13:01:35 +0600

    first commit

---
+++ internal/app/app.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"context"
+	"github.com/olezhek28/clean-architecture/internal/config"
+)
+
+type App struct {
+	serviceProvider *serviceProvider
+}
+
+func NewApp(ctx context.Context) (*App, error) {
+	a := &App{}
+
+	err := a.initDeps(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
+
+func (a *App) Run() error {
+	return a.runGRPCServer()
+}
+
+func (a *App) initDeps(ctx context.Context) error {
+	inits := []func(context.Context) error{
+		a.initConfig,
+		a.initServiceProvider,
+	}
+
+	for _, f := range inits {
+		err := f(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *App) initConfig(_ context.Context) error {
+	err := config.Load(".env")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) initServiceProvider(_ context.Context) error {
+	a.serviceProvider = newServiceProvider()
+	return nil
+}
